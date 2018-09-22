c4a9ab05318e1a93fb9b8accc9f4e9c7daad6d9b
vincentchan <everest115@outlook.com>
2018-09-21 21:49:13 -0700

    HashTable and HashSet

--- src/Hash/UncommonWord884.java
+++ src/Hash/UncommonWord884.java
@@ -11,37 +11,37 @@ public class UncommonWord884 {
 
 
     }
-    // Using HashMap
-//    public String[] uncommonFromSentences(String A, String B){
-//        String C = A + " " + B;
-//        Map<String,Integer> res = new HashMap<>();
-//        String[] st = C.split(" ");
-//        for (String word : st){
-//            res.put(word,res.getOrDefault(word,0) + 1);
-//        }
-//        List<String> result = new ArrayList<>();
-//        for (Map.Entry<String,Integer> entry: res.entrySet()){
-//            if (entry.getValue() == 1){
-//                result.add(entry.getKey());
-//            }
-//        }
-//        return result.toArray(new String[result.size()]);
-//
-//    }
-    // Using HashSet
-    public String[] uncommonFromSentences(String A, String B) {
+    // Using HashMap. Time complexity: O(n)
+    public String[] uncommonFromSentences(String A, String B){
         String C = A + " " + B;
+        Map<String,Integer> res = new HashMap<>();
         String[] st = C.split(" ");
-        Set<String> diff = new HashSet<>();
-        Set<String> common = new HashSet<>();
-        for (String word : st) {
-            if (common.contains(word) || !diff.add(word)){
-                diff.remove(word);
-                common.add(word);
+        for (String word : st){
+            res.put(word,res.getOrDefault(word,0) + 1);
+        }
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String,Integer> entry: res.entrySet()){
+            if (entry.getValue() == 1){
+                result.add(entry.getKey());
             }
         }
+        return result.toArray(new String[result.size()]);
 
-        List<String> res = new ArrayList<>(diff);
-        return res.toArray(new String[res.size()]);
     }
+    // Using HashSet
+//    public String[] uncommonFromSentences(String A, String B) {
+//        String C = A + " " + B;
+//        String[] st = C.split(" ");
+//        Set<String> diff = new HashSet<>();
+//        Set<String> common = new HashSet<>();
+//        for (String word : st) {
+//            if (common.contains(word) || !diff.add(word)){
+//                diff.remove(word);
+//                common.add(word);
+//            }
+//        }
+//
+//        List<String> res = new ArrayList<>(diff);
+//        return res.toArray(new String[res.size()]);
+//    }
 }
