8d37864685a4c298d01d6913d5a176fa21d897f7
weisongchen <chen.weis@husky.neu.edu>
2019-03-07 16:30:08 -0800

    Array modified

---
+++ src/Array/MinimumSizeSubarraySum209.java
@@ -0,0 +1,72 @@
+package Array;
+
+public class MinimumSizeSubarraySum209 {
+    public static void main(String[] args) {
+        MinimumSizeSubarraySum209 ms = new MinimumSizeSubarraySum209();
+        int[] nums = {2,3,1,2,4,3};
+        System.out.println(ms.minSubArrayLen(7, nums));
+    }
+    /**
+     * Using Binary Search
+     */
+    public int minSubArrayLen(int s, int[] nums){
+        int res = Integer.MAX_VALUE;
+        int[] sums = new int[nums.length + 1];
+        for (int i = 1; i < sums.length; i++){
+            sums[i] = sums[i - 1] + nums[i - 1];
+        }
+        for (int i = 0; i < sums.length; i++){
+            int end = binarySearch(i + 1, sums.length - 1, sums[i] + s, sums);
+            if (end == sums.length) break;
+            res = Math.min(res, end - i);
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+    public int binarySearch(int lo, int hi, int key, int[] nums){
+        while (lo <= hi){
+            int mid = (lo + hi) / 2;
+            if (key <= nums[mid]){
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+    /**
+     * TWO POINTER
+     * @param s
+     * @param nums
+     * @return
+     */
+//    public int minSubArrayLen(int s, int[] nums){
+//        int res = Integer.MAX_VALUE, left = 0, sum = 0;
+//        for (int i = 0; i < nums.length; i++){
+//            sum += nums[i];
+//            while (left <= i && sum >= s){
+//                res = Math.min(res, i - left + 1);
+//                sum -= nums[left];
+//                left++;
+//            }
+//        }
+//        return res == Integer.MAX_VALUE ? 0 : res;
+//    }
+//    public int minSubArrayLen(int s, int[] nums) {
+//        if (nums == null || nums.length == 0) return 0;
+//        int res = Integer.MAX_VALUE;
+//        int left = 0, right = 0, sum = 0, len = nums.length;
+//        while (right < len){
+//            while (sum < s && right < len){
+//                sum += nums[right];
+//                right++;
+//            }
+//            while (sum >= s){
+//                res = Math.min(res, right - left);
+//                sum -= nums[left];
+//                left++;
+//            }
+//        }
+//        return res == Integer.MAX_VALUE ? 0: res;
+//    }
+}
--- src/Array/PeakIndexArray852.java
+++ src/Array/PeakIndexArray852.java
@@ -1,36 +1,44 @@
 package Array;
 
+/**
+ * @Author Vincent
+ * @Date
+ * @Review 2019-3-7
+ */
 public class PeakIndexArray852 {
     public static void main(String[] args) {
         PeakIndexArray852 pa = new PeakIndexArray852();
-        int[] nums = {0,1,2,4,3,1,0};
-        int[] nums2 = {0,1,0};
+        int[] nums = {0, 2, 1, 4, 3, 1, 0};
+        int[] nums2 = {0, 1, 0};
         System.out.println(pa.peakIndexInMountainArray(nums2));
     }
 
     // Simple Solution
-//    public int peakIndexInMountainArray(int[] A){
-//        if (A.length == 1 || A == null) return 0;
-//        for (int i = 0; i < A.length; i++){
-//            if (A[i] > A[i+1]) return i;
-//        }
-//        return 0;
-//    }
-    // Binary Search
     public int peakIndexInMountainArray(int[] A) {
-        int left = 0, right = A.length - 1;
-
-        while(left < right){
-            int mid = left + (right - left) / 2;
-            if (A[mid] < A[mid - 1]){
-                right = mid;
-            }
-            else if (A[mid] < A[mid + 1]){
-                left = mid;
-            }
-            else return mid;
-
+        if (A.length == 1 || A == null) return 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > A[i + 1]) return i;
         }
         return 0;
     }
+    /**
+     * Binary Search
+     */
+
+//    public int peakIndexInMountainArray(int[] A) {
+//        int left = 0, right = A.length - 1;
+//
+//        while(left < right){
+//            int mid = left + (right - left) / 2;
+//            if (A[mid] < A[mid - 1]){
+//                right = mid;
+//            }
+//            else if (A[mid] < A[mid + 1]){
+//                left = mid;
+//            }
+//            else return mid;
+//
+//        }
+//        return 0;
+//    }
 }
